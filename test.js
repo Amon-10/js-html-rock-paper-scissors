8c819e79c34298389248834e13fe3281896f60ae
amanlal <alal09635@gmail.com>
2025-07-01 16:20:12 -0700

    created rock-paper-scissors array and random selection code with rock paper scissors buttons

---
+++ test.js
@@ -0,0 +1,13 @@
+const options = ['rock', 'paper', 'scissors'];
+
+const randomIndex = Math.floor(Math.random() * options.length);
+const randomOption = options[randomIndex];
+console.log(randomOption);
+
+/* if (button == 'Rock' && randomOption == 'scissors'){
+    alert('randomOption');
+    alert('You win');
+}
+else{
+    alert('You lose');
+} */
